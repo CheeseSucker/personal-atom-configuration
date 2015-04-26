23bd598346c3cc5957dba8b02f83371565ccf8c9
CheeseSucker <cheesesucker@gmail.com>
2015-04-26 21:55:26 +0200

    Don't include package directory in git.

--- packages/build/spec/fixture/Gruntfile.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(grunt) {
-  grunt.initConfig({});
-  grunt.registerTask('default', function() {
-    console.log('Surprising is the passing of time. But not so, as the time of passing');
-  });
-};
